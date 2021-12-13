8fb04d6cadfba5fc2670c8e074676d10b25430ec
lowyongting <ytlow1234@gmail.com>
2021-12-14 00:13:45 +0800

    Updates

---
+++ precache-manifest.ab326e35862e3c19cbfe7b3dd7f565d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8397eb66ac08e214d3c6553bc594ff4",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "f76b2333ad8ed351340b",
+    "url": "/personal-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3d76831c3a5d9b97d7a5",
+    "url": "/personal-portfolio/static/css/main.5ccb174b.chunk.css"
+  },
+  {
+    "revision": "f76b2333ad8ed351340b",
+    "url": "/personal-portfolio/static/js/2.b6b4cf5d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/personal-portfolio/static/js/2.b6b4cf5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d76831c3a5d9b97d7a5",
+    "url": "/personal-portfolio/static/js/main.5ab42bc0.chunk.js"
+  },
+  {
+    "revision": "b620e5e7f0587fbb1b94",
+    "url": "/personal-portfolio/static/js/runtime-main.3349a966.js"
+  }
+]);
